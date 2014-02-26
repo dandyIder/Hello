4763e715bc697aa4ea4a24dc76c3cb54877690bb
dandyIder <309380928@qq.com>
2014-02-26 22:31:59 +0800

    Stroy 2014-02-26 22:31

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+public HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+	}
+
+	public String toString(){
+		System.out.println("this HelloWord toString..")
+	}
+}
